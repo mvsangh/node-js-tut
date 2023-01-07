b1acb4961a64d7e3af12c4b95711709c25ea9f38
mvsangh <msangh.devloper@gmail.com>
2023-01-08 03:15:26 +0530

    Updated the db config

--- app/models/db.js
+++ app/models/db.js
@@ -5,7 +5,7 @@ const dbConnection = mysql.createConnection({
 	host:dbConfig.HOST,
 	user:dbConfig.USER,
 	password:dbConfig.PASSWORD,
-	database:dbConfig.DB
+	database:dbConfig.DB,
 	port:dbConfig.PORT
 
 });
